41aaa5cbfc9a1d5a2748efbca38a5d360b183755
michalby24 <michalby24@gmail.com>
2025-03-19 18:09:38 +0200

    refactor(actions): test update registry

--- actions/update-artifacts-file/modify_artifacts.sh
+++ actions/update-artifacts-file/modify_artifacts.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
-set -e
-
-# Split the scope input by commas and iterate over each element
-echo "$1" | tr ',' '\n' | while read -r element; do
-  # Navigate to the specific scope directory
-  pushd helm-charts/$element
+# Split string by comma
+echo "${{ inputs.scope }}" | tr ',' '\n' | while read -r element; do
+  # cd to directory
+  # pushd helm-charts/$element
+  pushd actions/.github/workflows/$element
   node -e "
   const fs = require('fs');
   const path = './artifacts.json';
   let data = {};
-  // Check if 'artifacts.json' exists and read its content
+  // Check if 'artifacts.json' file already exists
   if (fs.existsSync(path)) {
     data = JSON.parse(fs.readFileSync(path, 'utf8'));
   }
-  // Initialize nested structure if it doesn't exist
-  if (!data['$2']) data['$2'] = {};
-  if (!data['$2']['$3']) {
-    data['$2']['$3'] = {};
-  }
-  const compositeKey = '${element}/$4';
-  // Update the artifact tag
-  data['$2']['$3'][compositeKey] = '$5';
-  // Write the updated content back to 'artifacts.json'
+  // Ensure nested structure: docker -> registry -> scope -> artifact:tag
+  if (!data['${{ inputs.type }}']['${{ inputs.registry }}']) data['${{ inputs.type }}']['${{ inputs.registry }}'] = {};
+  const compositeKey = '${element}/${{ inputs.artifact-name }}';
+  // Assign the artifact tag to the composite key
+  data['${{ inputs.type }}']['${{ inputs.registry }}'][compositeKey] = '${{ inputs.artifact-tag }}';
+  // Writing new file contents
   fs.writeFileSync(path, JSON.stringify(data, null, 2));
   "
-  # Return to the previous directory
+  # go back to before pushd
   popd
 done
