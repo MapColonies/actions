71b56f2589b6e5373ccf050fe53f133121f919dd
michalby24 <michalby24@gmail.com>
2025-03-19 18:27:54 +0200

    refactor(actions): test update registry

--- actions/update-artifacts-file/modify_artifacts.sh
+++ actions/update-artifacts-file/modify_artifacts.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 # Split string by comma
-echo "$SCOPE" #| tr ',' '\n' | while read -r element; do
+echo "$SCOPE" | tr ',' '\n' | while read -r element; do
   # cd to directory
-  # pushd helm-charts/$element
-  # pushd actions/.github/workflows/$element
-  # node -e "
-  # const fs = require('fs');
-  # const path = './artifacts.json';
-  # let data = {};
-  # // Check if 'artifacts.json' file already exists
-  # if (fs.existsSync(path)) {
-  #   data = JSON.parse(fs.readFileSync(path, 'utf8'));
-  # }
-  # // Ensure nested structure: docker -> registry -> scope -> artifact:tag
-  # if (!data['${{ inputs.type }}']['${{ inputs.registry }}']) data['${{ inputs.type }}']['${{ inputs.registry }}'] = {};
-  # const compositeKey = '${element}/${{ inputs.artifact-name }}';
-  # // Assign the artifact tag to the composite key
-  # data['${{ inputs.type }}']['${{ inputs.registry }}'][compositeKey] = '${{ inputs.artifact-tag }}';
-  # // Writing new file contents
-  # fs.writeFileSync(path, JSON.stringify(data, null, 2));
-  # "
-  # # go back to before pushd
-  # popd
-# done
+  pushd helm-charts/$element
+  node -e "
+  const fs = require('fs');
+  const path = './artifacts.json';
+  let data = {};
+  // Check if 'artifacts.json' file already exists
+  if (fs.existsSync(path)) {
+    data = JSON.parse(fs.readFileSync(path, 'utf8'));
+  }
+  // Ensure nested structure: docker -> registry -> scope -> artifact:tag
+  if (!data['$TYPE']['$REGISTRY']) data['$TYPE']['$REGISTRY'] = {};
+  const compositeKey = '${element}/$ARTIFACT_NAME'
+  // Assign the artifact tag to the composite key
+  data['$TYPE']['$REGISTRY'][compositeKey] = '$ARTIFACT_TAG' }}';
+  // Writing new file contents
+  fs.writeFileSync(path, JSON.stringify(data, null, 2));
+  "
+  # go back to before pushd
+  popd
+done
