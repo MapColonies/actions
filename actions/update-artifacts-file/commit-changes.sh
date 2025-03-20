ed4f82ff854b0911a3e6b3dba12491df207bf4ee
michalby24 <michalby24@gmail.com>
2025-03-20 14:09:35 +0200

    refactor(actions): test update registry

--- actions/update-artifacts-file/commit-changes.sh
+++ actions/update-artifacts-file/commit-changes.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-# cd helm-charts
-# git config --global user.name "mapcolonies[bot]"
-# git config --global user.email "devops[bot]@mapcolonies.com"
+cd $SCOPE
+git config --global user.name "mapcolonies[bot]"
+git config --global user.email "devops[bot]@mapcolonies.com"
 
-# git add .
-# # Add "|| true" for not failing on this line
-# git commit -m "chore: update artifacts.json for ${{ inputs.scope }}" -m "with ${{ inputs.type }} artifact: ${{ inputs.artifact-name }}:${{ inputs.artifact-tag }}" || true
+git add .
+# Add "|| true" for not failing on this line
+git commit -m "chore: update artifacts.json for $SCOPE" -m "with $TYPE artifact: $ARTIFACT_NAME:$ARTIFACT_TAG" || true
 
-# success=false
-# for ((i=1; i<=5; i++)); do
-#   echo "Attempt number $i"
-#   if git push; then
-#     success=true
-#     break
-#   else
-#     sleep 5
-#     # --ff for fast-forward
-#     git pull origin master --ff
-#   fi
-# done
+success=false
+for ((i=1; i<=5; i++)); do
+  echo "Attempt number $i"
+  if git push; then
+    success=true
+    break
+  else
+    sleep 5
+    # --ff for fast-forward
+    git pull origin master --ff
+  fi
+done
 
-# if [ "$success" = false ]; then
-#   echo "Failed to push changes."
-#   exit 1
-# fi
+if [ "$success" = false ]; then
+  echo "Failed to push changes."
+  exit 1
+fi
