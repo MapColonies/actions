5c4381502a87ee05ccb09e808b58162fe7d78057
michalby24 <michalby24@gmail.com>
2025-07-01 19:30:35 +0300

    fix(update-artifacts-file): testing

--- actions/update-artifacts-file/commit_changes.sh
+++ actions/update-artifacts-file/commit_changes.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
-set -e
-
-cd "$TARGET_DIR"
-
-git config user.name "mapcolonies[bot]"
-git config user.email "devops[bot]@mapcolonies.com"
+cd $DOMAIN
+git config --global user.name "mapcolonies[bot]"
+git config --global user.email "devops[bot]@mapcolonies.com"
 
 git add "$DOMAIN/artifacts.json"
-git commit -m "chore: update artifacts.json for $DOMAIN" -m "with $TYPE artifact: $ARTIFACT_NAME:$ARTIFACT_TAG" || echo "Nothing to commit"
+# Add "|| true" for not failing on this line 1
+git commit -m "chore: update artifacts.json for $DOMAIN" -m "with $TYPE artifact: $ARTIFACT_NAME:$ARTIFACT_TAG" || true
 
 success=false
 for ((i=1; i<=5; i++)); do
-  echo "Attempt $i to push changes..."
-  if git push https://x-access-token:$GITHUB_TOKEN@github.com/$TARGET_REPO.git; then
-    success=true
-    echo "Push succeeded"
+  echo "Attempt number $i"
+  if git push https://x-access-token:$GITHUB_TOKEN@github.com/$TARGET_REPO.git; then    success=true
     break
   else
-    echo "⚠️ Push failed, retrying after pull"
-    git pull origin main --ff-only || true
     sleep 5
+    # --ff for fast-forward
+    git pull origin master --ff
   fi
 done
 
 if [ "$success" = false ]; then
-  echo "Failed to push changes after 5 attempts."
+  echo "Failed to push changes."
   exit 1
 fi
